5feb3544d9e134b0b6cb01263a61f939dd014cba
Ben Johnson <bjohnson@binarylogic.com>
2013-06-17 13:18:54 -0400

    fix making off of associations

--- lib/machinist/active_record/lathe.rb
+++ lib/machinist/active_record/lathe.rb
@@ -3,18 +3,26 @@ module Machinist::ActiveRecord
   class Lathe < Machinist::Lathe
 
     def make_one_value(attribute, args) #:nodoc:
-      if block_given?
+      value = if block_given?
         raise_argument_error(attribute) unless args.empty?
         yield
       else
         make_association(attribute, args)
       end
+
+      # save the associations if we are doing a hard make!
+      if Thread.current["#{klass.name}_hard_save"] && value.is_A?(ActiveRecord::Base)
+        value.save!
+      end
+
+      value
     end
 
     def make_association(attribute, args) #:nodoc:
       association = @klass.reflect_on_association(attribute)
       if association
-        association.klass.make(*args)
+        # only make the association if neccessary
+        object.send(attribute) || association.klass.make(*args)
       else
         raise_argument_error(attribute)
       end
